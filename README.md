94fa724e98d94ef0f6978e6ab670b925676211bc
Avash Adhikari <avashraj328@gmail.com>
2026-06-25 11:54:27 -0700

    api[feat]: simple twiML endpoint

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ An AI voice bot used to test Pretty Good AI's AI voice bot to find bugs and eval
 
 ## How to use
 1. `uv run fastapi dev`
+2. `ngrok http 8000`
--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from fastapi.responses import Response
+from twilio.twiml.voice_response import VoiceResponse
+
+def say_something(message: str = "Hello from Twlilio!"):
+    response = VoiceResponse()
+    response.say(message)
+
+    return str(response)
 
 app = FastAPI()
 
 @app.get("/health")
 async def health():
     return {"message": "healthy"}
+
+@app.get("/voice")
+async def voice():
+    return Response(content=say_something(), media_type="application/xml")
